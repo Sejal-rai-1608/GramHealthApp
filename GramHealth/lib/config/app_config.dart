b6e792563eb6e3917ff60b281c9c4d8e5c979720
Vilas Oza <vilasoza19@gmail.com>
2026-09-02 20:29:51 +0530

    your commit message

--- GramHealth/lib/config/app_config.dart
+++ GramHealth/lib/config/app_config.dart
@@ -12,7 +12,7 @@ class AppConfig {
   static String get baseUrl {
     if (kIsWeb) return 'http://localhost:5000';
     if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:5000';
+      return 'http://192.168.1.7:5000';
     }
     return 'http://localhost:5000';
   }
