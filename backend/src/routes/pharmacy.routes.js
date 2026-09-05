2bc413f2885cafde761cd0ef08c1f61d2dfb59f0
Vilas Oza <vilasoza19@gmail.com>
2026-09-05 21:56:40 +0530

    fix: pharmacy routes auth middleware import path

--- backend/src/routes/pharmacy.routes.js
+++ backend/src/routes/pharmacy.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pharmacyController = require("../controllers/pharmacy.controller");
-const { protect, authorize } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth.middleware");
 
 // Public (or just protected) route to sync pharmacy data offline
 router.get("/list", protect, pharmacyController.list);
